7af7a3ece032b73f4d46382f23254c04af690d49
Clem Blanco <clement.blanco@pod-point.com>
2022-02-22 12:22:00 +0000

    Revert back to a Factory to prevent IoC memory leaks

--- src/MonologKinesisServiceProvider.php
+++ src/MonologKinesisServiceProvider.php
@@ -22,7 +22,7 @@ class MonologKinesisServiceProvider extends ServiceProvider
 
         $this->app->resolving(LogManager::class, function (LogManager $manager) {
             $manager->extend('kinesis', function (Container $app, array $config) {
-                return $this->createKinesisLogger($app, $config);
+                return (new LoggerFactory($app))->create($config);
             });
         });
     }
@@ -35,37 +35,5 @@ class MonologKinesisServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-    }
-
-    /**
-     * Create a new instance of a Kinesis logger.
-     *
-     * @param  Container  $app
-     * @param  array  $config
-     * @return Logger
-     *
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
-     */
-    private function createKinesisLogger(Container $app, array $config): Logger
-    {
-        $client = $app->make(Client::class)->configure($config);
-        $level = Arr::get($config, 'level', Logger::DEBUG);
-
-        $kinesisHandler = new Handler($client, $config['stream'], $level);
-        $kinesisHandler->setFormatter($this->createKinesisFormatter($app));
-
-        return new Logger('kinesis', [$kinesisHandler]);
-    }
-
-    /**
-     * Create a formatter for a Kinesis logger.
-     *
-     * @param  Container  $app
-     * @return Formatter
-     */
-    private function createKinesisFormatter(Container $app): Formatter
-    {
-        /** @var \Illuminate\Contracts\Foundation\Application $app */
-        return new Formatter($app['config']->get('app.name'), $app->environment());
     }
 }
