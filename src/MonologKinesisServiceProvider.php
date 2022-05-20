f82a1a4d3711a76fdcd12dd2f32fd179a598039a
David França <davidjonasmarques@gmail.com>
2022-05-20 11:12:34 -0300

    apply psr11, should not pass a container into an object

--- src/MonologKinesisServiceProvider.php
+++ src/MonologKinesisServiceProvider.php
@@ -20,7 +20,13 @@ class MonologKinesisServiceProvider extends ServiceProvider
 
         $this->app->resolving(LogManager::class, function (LogManager $manager) {
             $manager->extend('kinesis', function (Container $app, array $config) {
-                return (new LoggerFactory($app))->create($config);
+                $client = $app->make(Client::class);
+
+                $name = $app['config']->get('app.name');
+                $environment = $app->environment();
+                $formatter = new Formatter($name, $environment);
+
+                return (new LoggerFactory($client, $formatter))->create($config);
             });
         });
     }
