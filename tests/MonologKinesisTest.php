76e8b3c123ad7d1e35d29d303166c30e97b72e88
Clem Blanco <clement.blanco@pod-point.com>
2022-10-05 13:31:59 +0200

    WIP

--- tests/MonologKinesisTest.php
+++ tests/MonologKinesisTest.php
@@ -68,8 +68,6 @@ class MonologKinesisTest extends TestCase
 
             $data = json_decode($argument['Data'], true);
 
-            dd($data, $argument['PartitionKey'], $argument['StreamName']);
-
             $hasJsonKeys = Arr::has($data, [
                 'timestamp', 'host', 'project', 'env', 'message',
                 'channel', 'level', 'extra', 'context',
